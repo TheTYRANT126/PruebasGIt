f82893bffef5accc02132b3548d90b480dbac802
TheTYRANT126 <spidermanvenom1206@hotmail.com>
2025-08-28 11:09:19 -0600

    Cambiar operación de suma a resta

--- script1.js
+++ script1.js
@@ -2,8 +2,8 @@
 let num1 = 5;
 let num2 = 7;
 
-// Sumar
-let suma = num1 + num2;
+// Restar
+let resta = num1 - num2;
 
 // Mostrar resultado en la consola
-console.log("La suma de", num1, "y", num2, "es:", suma);
+console.log("La resta de", num1, "y", num2, "es:", resta);

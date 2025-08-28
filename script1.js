61b124f607775fe04a5d333ab9697d7d4b2d4122
TheTYRANT126 <spidermanvenom1206@hotmail.com>
2025-08-28 11:25:45 -0600

    ahora los números se multiplican

--- script1.js
+++ script1.js
@@ -2,8 +2,8 @@
 let num1 = 5;
 let num2 = 7;
 
-// Restar
-let resta = num1 - num2;
+// Multiplicar
+let multiplicacion = num1 * num2;
 
 // Mostrar resultado en la consola
-console.log("La resta de", num1, "y", num2, "es:", resta);
+console.log("La multiplicación de", num1, "y", num2, "es:", multiplicacion);

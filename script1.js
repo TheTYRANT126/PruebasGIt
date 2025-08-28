d1ebba430018988bc4a0c8f20da803f729654921
TheTYRANT126 <spidermanvenom1206@hotmail.com>
2025-08-28 11:04:12 -0600

    Primer commit

---
+++ script1.js
@@ -0,0 +1,9 @@
+// Definir dos números
+let num1 = 5;
+let num2 = 7;
+
+// Sumar
+let suma = num1 + num2;
+
+// Mostrar resultado en la consola
+console.log("La suma de", num1, "y", num2, "es:", suma);

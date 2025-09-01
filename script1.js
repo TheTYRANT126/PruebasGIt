dfa74541953917bb784a1b09f27fa7958202696f
TheTYRANT126 <spidermanvenom1206@hotmail.com>
2025-09-01 11:31:55 -0600

    Antes de revert 2

--- script1.js
+++ script1.js
@@ -8,4 +8,8 @@ let multiplicacion = num1 * num2;
 // Mostrar resultado en la consola
 console.log("La multiplicación de", num1, "y", num2, "es:", multiplicacion);
 
-//quiero hacer un nuevo comit 
+//quiero hacer un nuevo comit
+
+//soy un comentario antes de nuevo revert
+let num3 = 13
+    ;

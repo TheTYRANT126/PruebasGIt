69d810c65c8247da9b0e686af6bd7b5386aaf4a6
TheTYRANT126 <spidermanvenom1206@hotmail.com>
2025-09-02 13:24:34 -0600

    Revert "Antes de revert 3"
    This reverts commit dfa74541953917bb784a1b09f27fa7958202696f.

--- script1.js
+++ script1.js
@@ -8,11 +8,4 @@ let multiplicacion = num1 * num2;
 // Mostrar resultado en la consola
 console.log("La multiplicación de", num1, "y", num2, "es:", multiplicacion);
 
-//quiero hacer un nuevo comit
-
-//soy un comentario antes de nuevo revert
-let num3 = 13
-    ;
-
-
-// nueva linea de códgio antes de revert
+//quiero hacer un nuevo comit intento 2
